15e6cd7b3859bc34950d44707d1605e8d382cb1b
Ubuntu <ubuntu@ip-172-31-47-31.us-west-2.compute.internal>
2013-08-18 21:14:17 +0000

    try file read again - forgot git add

--- web.js
+++ web.js
@@ -1,12 +1,43 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+// read helloworld string from file
+
+
+var fileName = "index.html";
+fs.exists(fileName, function(exists) {
+  if (exists) {
+    fs.stat(fileName, function(error, stats) {
+      fs.open(fileName, "r", function(error, fd) {
+        var buffer = new Buffer(stats.size);
+        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+          var data = buffer.toString("utf8", 0, buffer.length);
+          console.log(data);
+
+          app.get('/', function(request, response) {
+            response.send(data);
+          });
+
+
+          fs.close(fd);
+        });
+      });
+    });
+  }
 });
+
+
+
+
+//app.get('/', function(request, response) {
+//  response.send('Hello World 2!');
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
